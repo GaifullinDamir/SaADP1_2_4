38f30ff33082148021b5963ad772b527f39203fd
Damir <damiros.20002@yandex.ru>
2022-03-12 21:27:46 +0300

    V0. ...

--- SaADP/main.cpp
+++ SaADP/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 
+
+using namespace std;
+
+struct StackItem
+{
+	int data;
+	StackItem* previous;
+};
+
+void init(StackItem* head) {
+	head = NULL;
+}
+
+bool isEmpty(StackItem* head) {
+	if (head == NULL)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void printStack(StackItem* head) {
+	if (isEmpty(head))
+	{
+		cout << "Stack is empty" << endl;
+	}
+
+	else
+	{
+		StackItem* current = head;
+		while(current != NULL)
+		{ 
+			cout << current->data << endl;
+			current = current->previous;
+		}
+	}
+	
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	StackItem* head = NULL;
+	init(head);
+	printStack(head);
 }
--- SaADP/stackitem.h
+++ SaADP/stackitem.h
@@ -1,18 +1,19 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-
-struct StackItem
-{
-	int data;
-	StackItem* previous, * head; //указатель на пред-й эл-т и на голову
-};
-
-StackItem* init();
-
-void add(int data, StackItem*& stack);
-
-void deleteItem(StackItem* stack);
-
-void show(StackItem* stack)
+//#pragma once
+//#include <iostream>
+//#include <string>
+//
+//
+//struct StackItem
+//{
+//	int data;
+//	StackItem* previous; 
+//};
+//
+//StackItem* init();
+//
+//void add(int data, StackItem*& stack);
+//
+//void deleteItem(StackItem* stack);
+//
+//void show(StackItem* stack);
+//
