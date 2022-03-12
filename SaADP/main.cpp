979d41dfe9bd01a9c1bf3bc62f3a9bec33e46ad1
Damir <damiros.20002@yandex.ru>
2022-03-12 22:24:16 +0300

    V0. ...

--- SaADP/main.cpp
+++ SaADP/main.cpp
@@ -9,11 +9,13 @@ struct StackItem
 	StackItem* previous;
 };
 
-void init(StackItem* head) {
+void init(StackItem* head) 
+{
 	head = NULL;
 }
 
-bool isEmpty(StackItem* head) {
+bool isEmpty(StackItem* head) 
+{
 	if (head == NULL)
 	{
 		return true;
@@ -24,7 +26,8 @@ bool isEmpty(StackItem* head) {
 	}
 }
 
-void printStack(StackItem* head) {
+void printStack(StackItem* head) 
+{
 	if (isEmpty(head))
 	{
 		cout << "Stack is empty" << endl;
@@ -41,9 +44,91 @@ void printStack(StackItem* head) {
 	}
 	
 }
+
+void addItem(StackItem*& head, int data)
+{
+	try
+	{
+		StackItem* current = new StackItem;
+		current->previous = head;
+		current->data = data;
+		head = current;
+	}
+	catch (const std::exception&)
+	{
+		cout << "Стек заполнен" << endl;
+	}
+}
+
+void addMultipleItems(StackItem*& head, int amount)
+{
+	for (int i = 0; i < amount; i++)
+	{
+		int data = rand();
+		addItem(head, data);
+	}
+}
+
+void deleteItem(StackItem*& head)
+{
+	if (!isEmpty(head))
+	{
+		StackItem* temp = head;
+		head = head->previous;
+		delete temp;
+	}
+	else
+	{
+		cout << "Стек пустой" << endl;
+	}
+}
+
+void moveToSecond(StackItem*& head1, StackItem*& head2)
+{
+	StackItem* temp = head1;
+	head1 = head1->previous;
+	temp->previous = head2;
+	head2 = temp;
+}
+
+void moveToFirst(StackItem*& head1, StackItem*& head2)
+{
+	StackItem* temp = head2;
+	head2 = head2->previous;
+	temp->previous = head1;
+	head1 = temp;
+}
 int main()
 {
-	StackItem* head = NULL;
-	init(head);
-	printStack(head);
+	StackItem* head1 = NULL;
+	StackItem* head2 = NULL;
+	init(head1);
+	addMultipleItems(head1, 5);
+	cout << endl;
+	cout << "head1 old" << endl;
+	cout << endl;
+	printStack(head1);
+	moveToSecond(head1, head2);
+	moveToSecond(head1, head2);
+	cout << endl;
+	cout << "head2" << endl;
+	cout << endl;
+	printStack(head2);
+	cout << endl;
+	cout << "head1 new" << endl;
+	cout << endl;
+	printStack(head1);
+	cout << endl;
+	moveToFirst(head1, head2);
+	moveToFirst(head1, head2);
+	cout << endl;
+	cout << "head 1 newNew" << endl;
+	cout << endl;
+	printStack(head1);
+
+
+	/*deleteItem(head);
+	deleteItem(head);
+	cout << " " << endl;
+	printStack(head);*/
 }
